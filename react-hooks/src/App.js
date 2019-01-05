64b07eee540e6d87ff9bf686b25a2f48751fa706
Jez <jez@jezfx.com>
2019-01-05 20:44:01 +0800

    add sharing logic

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => setCount(count + 1);
+function useCounter({ initialState, step }) {
+  const [count, setCount] = useState(initialState);
+  const incrementCount = () => setCount(count + step);
+  return { count, incrementCount };
+}
 
+function App() {
+  const { count, incrementCount } = useCounter({ initialState: 5, step: 3 });
   return (
     <div className="container">
       <button className="button" onClick={incrementCount}>
