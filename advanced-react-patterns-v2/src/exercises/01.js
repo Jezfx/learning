59cfe46988301376b18f19e4299ad87ac1076b91
Jez <jez@jezfx.com>
2019-01-11 17:21:27 +0800

    add advanced react components files

---
+++ advanced-react-patterns-v2/src/exercises/01.js
@@ -0,0 +1,48 @@
+// Building the toggle component
+
+import React from 'react'
+import {Switch} from '../switch'
+
+class Toggle extends React.Component {
+  // 🐨 this toggle component is going to need to have state for `on`
+  state = {
+    on: false,
+  }
+
+  //
+  // You'll also want a method to handle when the switch is clicked
+  // which will update the `on` state and call the `onToggle` prop
+  // with the new `on` state.
+  handleSwitchOnClick = () => {
+    // 💯 Use a state updater function for `newState` to avoid issues with batching
+    this.setState(
+      state => {
+        return {on: !state.on}
+      },
+      () => {
+        // 💰 this.setState(newState, callback)
+        // The `callback` should be where you call `this.props.onToggle(this.state.on)`
+        this.props.onToggle(this.state.on)
+      },
+    )
+  }
+  //
+  render() {
+    // 🐨 here you'll want to return the switch with the `on` and `onClick` props
+    return (
+      <Switch onClick={this.handleSwitchOnClick} on={this.state.on} />
+    )
+  }
+}
+
+// Don't make changes to the Usage component. It's here to show you how your
+// component is intended to be used and is used in the tests.
+// You can make all the tests pass by updating the Toggle component.
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return <Toggle onToggle={onToggle} />
+}
+Usage.title = 'Build Toggle'
+
+export {Toggle, Usage as default}
